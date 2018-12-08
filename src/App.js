5b0727a199b84e999a60fcb1e5002a2c5dafa1f3
Berkay GÜRE <bgure@hotmail.com.tr>
2018-12-08 19:31:44 +0300

    Adds tailwind cs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, {Component} from 'react';
-import {Provider} from 'react-redux';
+import React from 'react';
+import { Provider } from 'react-redux';
 import ReportDesigner from './ReportDesigner';
 import store from './redux/store';
 
-class App extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    componentWillMount() {
-
-    }
-
-    render() {
-        return (
-            <Provider store={store}>
-                <ReportDesigner />
-            </Provider>
-        );
-    }
-}
+const App = () => (
+    <Provider store={ store }>
+        <ReportDesigner />
+    </Provider>
+)
 
 export default App;
--- src/ReportDesigner.js
+++ src/ReportDesigner.js
@@ -2,52 +2,11 @@ import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import Editor from './components/Editor/Editor';
-import Toolbox from './components/Toolbox';
-import Properties from './components/Properties';
 
-import {
-    faChevronLeft,
-    faCog,
-    faPlay,
-    faLayerGroup,
-    faToolbox,
-    faMousePointer,
-    faFont,
-    faTable,
-    faImage,
-    faProjectDiagram
-}  from '@fortawesome/free-solid-svg-icons';
-
-library.add(
-    fab,
-    faChevronLeft,
-    faCog, faPlay,
-    faLayerGroup,
-    faToolbox,
-    faMousePointer,
-    faFont,
-    faTable,
-    faImage,
-    faProjectDiagram
-);
-
-class ReportDesigner extends Component {
-    render() {
-        return (
-            <div className="app">
-                <Toolbox />
-                <div className="designer-area">
-                    <div className="designer-title">
-                        Designer Title
-                    </div>
-                    <div className="editor-container">
-                        <Editor width={ 672 } height={ 950 }/>
-                    </div>
-                </div>
-                <Properties />
-            </div>
-        );
-    }
-}
+const ReportDesigner = () => (
+    <div>
+        <Editor width={ 672 } height={ 950 }/>
+    </div>
+)
 
 export default ReportDesigner;
