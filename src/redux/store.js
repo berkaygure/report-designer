aec462d88d2b15c5c8bf11888b2ae71c5f354c69
Berkay GÜRE <bgure@hotmail.com.tr>
2018-12-11 19:47:30 +0300

    wip

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ const middleware = [ thunk ];
 
 let allEnhancers;
 
-if (console.log(process.env.NODE_ENV === 'development')) {
+if (process.env.NODE_ENV === 'development') {
   allEnhancers = compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
